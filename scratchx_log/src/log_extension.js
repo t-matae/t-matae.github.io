d23ddac49d85cf09906324458de9298559639034
俣江忠 <matae@yupiteru.plus>
2018-06-26 14:19:38 +0900

    cosme

--- scratchx_log/src/log_extension.js
+++ scratchx_log/src/log_extension.js
@@ -1,8 +1,6 @@
 (function(ext) {
-  // コメントは補足の説明なので書かなくてもいいです
 
   // Extension が終了するときに呼ばれる
-  // 今は特に何もしない
   ext._shutdown = function() {};
 
   // Extension の状態を返す
@@ -13,101 +11,78 @@
   //      1,       yellow, not ready
   //      2,        green, ready
   // ---
-  // 今回はデバイスなどは使用しないので常に準備完了
-  // ということで 2 を返します。
+  // 今回はデバイスの状態は考慮しないので 2,Ready にする
   ext._getStatus = function() {
     return {status: 2, msg: 'Ready'};
   };
 
-  //////////
-  // ブロックの処理
-  /////
 
+  //////////
+  // 各ブロックの処理
+  //////////
+
+  // debug用
   // no operation
   ext.no_operation = () => {
      // no operation
   };
 
-
-  // log表示
-  ext.log = function(str) {
-    alert(str);       // ログをアラート表示するだけ
+  // ScratchX上でlog表示
+  ext.log_alert = (str) => {
+    alert(str);         // ログをアラート表示するだけ
   };
 
-  //postのテストブロック
-  ext.post_test = function() {
-        // Make an AJAX call to the Open Weather Maps API
-        // var send_data = JSON.stringify({jobs:[{operation:{startTime:2017-12-22 14:20:38.688,touchFlag:false,actions:[{x:10,y:0,z:0,speed:0.5,emotion:normal,gaze:on,cheeks:red,eyecolor:aqua,complexioncolor:red,transmittance:0,delay:0,reset:false,talk:}]},schedule:* * * * * *,topic:yupibow2}]});
-        // var send_data = JSON.stringify({x:10,y:0,z:0});
-        // var send_data = JSON.stringify({x:0,y:0,z:0,speed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:"0",delay:"0",reset:"false",talk:"初期位置！"});
+  // ユピ坊制御
+  // 初期位置
+  ext.move_to_init = function() {
 
-        var send_data = JSON.stringify({jobs:[{operation:{startTime:"2017-12-22 14:20:38.688",touchFlag:"false",actions:[{x:0,y:0,z:0,speed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false",talk:"初期位置に移動！"}]},schedule:"******",topic:"yupibow1"}]});
+    var send_data = JSON.stringify({jobs:[{operation:{startTime:"2017-12-22 14:20:38.688",touchFlag:"false",actions:[{x:0,y:0,z:0,speed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false",talk:"初期位置に移動！"}]},schedule:"******",topic:"yupibow1"}]});
 
-        $.ajax({
-              // url: 'http://192.168.22.108:8080/',
-              url: 'http://localhost:8080//',
+    $.ajax({
+      url: 'http://localhost:8080//',
+      type: 'POST',
+      dataType: 'json',
+      data: send_data,
+    });
+  };
 
-              // url: 'https://www.muryou-tools.com/test/aaaa.php',
-              type: 'POST',
-              dataType: 'json',
-              data: send_data,
+  // 顔を上げる
+  ext.move_to_up = function() {
 
-        });
-    };
-    // // postのテストブロック
-    ext.move_to_up = function() {
-          // Make an AJAX call to the Open Weather Maps API
-          // var json_data = JSON.stringify({"jobs":[{"operation":{"startTime":"2017-12-22 14:20:38.688","touchFlag":"false","actions":[{"x":10,"y":0,"z":0,"speed":0.5,"emotion":"normal","gaze":"on","cheeks":"red","eyecolor":"aqua","complexioncolor":"red","transmittance":0,"delay":0,"reset":"false","talk":""}]},"schedule":"* * * * * *","topic":"yupibow2"}]});
+    var send_data = JSON.stringify({jobs:[{operation:{startTime:"2017-12-22 14:20:38.688",touchFlag:"false",actions:[{x:0,y:7,z:0,speed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false",talk:"上に移動！"}]},schedule:"* * * * * *",topic:"yupibow1"}]});
 
-          var send_data = JSON.stringify({jobs:[{operation:{startTime:"2017-12-22 14:20:38.688",touchFlag:"false",actions:[{x:0,y:7,z:0,speed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false",talk:"上に移動！"}]},schedule:"* * * * * *",topic:"yupibow1"}]});
+    $.ajax({
+      url: 'http://localhost:8080//',
+      type: 'POST',
+      dataType: 'json',
+      data: send_data,
+    });
+  };
 
-          $.ajax({
-                // url: 'http://192.168.22.108:8080/',
-                url: 'http://localhost:8080//',
+  // postのテストブロック
+  ext.move_to_down = function() {
 
-                // url: 'https://www.muryou-tools.com/test/aaaa.php',
-                type: 'POST',
-                dataType: 'json',
-                data: send_data,
+    var send_data = JSON.stringify({jobs:[{operation:{startTime:"2017-12-22 14:20:38.688",touchFlag:"false",actions:[{x:0,y:-30,z:0,speed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false",talk:"下に移動！"}]},schedule:"* * * * * *",topic:"yupibow1"}]});
 
-          });
-      };
+    $.ajax({
+      url: 'http://localhost:8080//',
+      type: 'POST',
+      dataType: 'json',
+      data: send_data,
+    });
+  };
 
-      // // postのテストブロック
-      ext.move_to_down = function() {
-            // Make an AJAX call to the Open Weather Maps API
-
-            // var json_data = JSON.stringify({"jobs":[{"operation":{"startTime":"2017-12-22 14:20:38.688","touchFlag":"false","actions":[{"x":-30,"y":0,"z":0,"speed":0.5,"emotion":"normal","gaze":"on","cheeks":"red","eyecolor":"aqua","complexioncolor":"red","transmittance":0,"delay":0,"reset":"false","talk":""}]},"schedule":"* * * * * *","topic":"yupibow2"}]});
-            var send_data = JSON.stringify({jobs:[{operation:{startTime:"2017-12-22 14:20:38.688",touchFlag:"false",actions:[{x:0,y:-30,z:0,speed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false",talk:"下に移動！"}]},schedule:"* * * * * *",topic:"yupibow1"}]});
-
-            $.ajax({
-                  // url: 'http://192.168.22.108:8080/',
-                  url: 'http://localhost:8080//',
-
-                  type: 'POST',
-                  dataType: 'json',
-                  data: send_data,
-
-            });
-        };
-
-  // ブロックをどういう風に表示するかを書きます
-  // ここの書き方は結構難しいので今は説明しません
+  // ブロック表示
   var descriptor = {
     blocks: [
-      // [' ', 'ログ表示(同期) %s',              'log', 'アラートでログを出力します'],
-      // ['w', 'ログ表示(非同期) %s',            'log', 'アラートでログを出力します'],
-      // [' ', 'websocet:post (同期)',         'post_test'],
-      // ['w', 'websocet:post (非同期)',         'post_test'],
-      [' ', '%m.move_to_direction に動かす', 'no_operation','体を右に'],
-      [' ', '%s としゃべらせる',               'no_operation', 'ぼく、ユピ坊！'],
-      [' ', 'up',         'move_to_up'],
-      [' ', 'down',         'move_to_down'],
-      [' ', 'post_test',         'post_test'],
+      [' ', '%m.move_to_direction に動かす', 'no_operation',  '体を右に'      ],
+      [' ', '%s としゃべらせる',              'no_operation',  'ぼく、ユピ坊！' ],
+      [' ', 'up',                           'move_to_up'    ],
+      [' ', 'down',                         'move_to_down'  ],
+      [' ', '初期位置に戻る',                 'move_to_init'  ],
     ],
     menus: {
       move_to_direction: ['顔を上に','顔を下に','顔を右に','顔を左に','体を右に','体を左に'],
-      lessMore: ['<', '>']
     },
   };
 

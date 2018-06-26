b1ecdfaaae1bb716ba1b8a712116c032bbcfb72f
俣江忠 <matae@yupiteru.plus>
2018-06-26 16:00:09 +0900

    Test

--- scratchx_log/src/log_extension.js
+++ scratchx_log/src/log_extension.js
@@ -32,9 +32,36 @@
     alert(str);         // ログをアラート表示するだけ
   };
 
+  ext.action_setting = (arg_x,arg_y,arg_z,arg_speed,arg_talk,arg_face) => {
+    let tempate_message = {jobs:[{operation:{startTime:"2017-12-2214:20:38.688",touchFlag:"false",actions:[{x:0,y:0,z:0,speed:0.5,emotion:"normal",talk:"初期位置に移動！",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false"}]},schedule:"******",topic:"yupibow2"}]};
+    alert(JSON.stringify(tempate_message));
+
+    let change_actions = {x:arg_x,y:arg_y,z:arg_z,speed:arg_speed,emotion:arg_face,talk:arg_talk};
+    alert(JSON.stringify(change_actions));
+    let setting_message = tempate_message.replace('x:0,y:0,z:0,speed:0.5,emotion:"normal",talk:"初期位置に移動！"',JSON.stringify(change_actions));
+
+    alert(JSON.stringify(setting_message));
+    return setting_json_message;
+  }
+
+
   // ユピ坊制御
+  // しゃべらせる
+  ext.speak_sentences = (sentences) => {
+ alert(tempate_json_message);
+    var send_data = JSON.stringify(tempate_json_message.replace('sentences',sentences));
+    alert(send_data);
+
+    $.ajax({
+      url: 'http://localhost:8080//',
+      type: 'POST',
+      dataType: 'json',
+      data: send_data,
+    });
+  };
+
   // 初期位置
-  ext.move_to_init = function() {
+  ext.move_to_init = () => {
 
     var send_data = JSON.stringify({jobs:[{operation:{startTime:"2017-12-22 14:20:38.688",touchFlag:"false",actions:[{x:0,y:0,z:0,speed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false",talk:"初期位置に移動！"}]},schedule:"******",topic:"yupibow1"}]});
 
@@ -47,7 +74,7 @@
   };
 
   // 顔を上げる
-  ext.move_to_up = function() {
+  ext.move_to_up = () => {
 
     var send_data = JSON.stringify({jobs:[{operation:{startTime:"2017-12-22 14:20:38.688",touchFlag:"false",actions:[{x:0,y:7,z:0,speed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false",talk:"上に移動！"}]},schedule:"* * * * * *",topic:"yupibow1"}]});
 
@@ -59,10 +86,10 @@
     });
   };
 
-  // postのテストブロック
-  ext.move_to_down = function() {
+  // 顔を下げる
+  ext.move_to_down = () => {
 
-    var send_data = JSON.stringify({jobs:[{operation:{startTime:"2017-12-22 14:20:38.688",touchFlag:"false",actions:[{x:0,y:-30,z:0,speed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false",talk:"下に移動！"}]},schedule:"* * * * * *",topic:"yupibow1"}]});
+    var send_data = JSON.stringify({jobs:[{operation:{startTime:"2017-12-22 14:20:38.688",touchFlag:"false",actions:[{x:0,y:-30,z:0,speeeeeed:0.5,emotion:"normal",gaze:"on",cheeks:"red",eyecolor:"aqua",complexioncolor:"red",transmittance:0,delay:0,reset:"false",talk:"下に移動！"}]},schedule:"* * * * * *",topic:"yupibow1"}]});
 
     $.ajax({
       url: 'http://localhost:8080//',
@@ -76,10 +103,12 @@
   var descriptor = {
     blocks: [
       [' ', '%m.move_to_direction に動かす', 'no_operation',  '体を右に'      ],
-      [' ', '%s としゃべらせる',              'no_operation',  'ぼく、ユピ坊！' ],
+      [' ', '%s としゃべらせる',              'speak_sentences',  'ぼく、ユピ坊！' ],
       [' ', 'up',                           'move_to_up'    ],
       [' ', 'down',                         'move_to_down'  ],
       [' ', '初期位置に戻る',                 'move_to_init'  ],
+      [' ', 'テストコマンド',                 'action_setting' ,1,1,1,1,1,1 ],
+
     ],
     menus: {
       move_to_direction: ['顔を上に','顔を下に','顔を右に','顔を左に','体を右に','体を左に'],
